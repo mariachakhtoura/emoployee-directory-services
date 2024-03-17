40cf449eca02ff6be5962768064d95d148103dc8
mariachakhtoura <38716626+mariachakhtoura@users.noreply.github.com>
2024-03-17 20:34:17 +0200

    MC - Add default route message

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const schema = require('./schema')
 const app = express()
 
 app.all('/graphql', createHandler({ schema }))
-
+app.get('/', (req, res) => {
+  res.send('Go to /playground to check the GrahQL Schema')
+})
 app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
 app.listen(3000)
